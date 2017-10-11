96eee5a6d0ae13d5df2032ab66a3de0e4068bb35
Piotrek <probi@ojooo.com>
2017-10-11 20:20:14 +0200

    clusters and plans unique slug added

--- updates/builder_table_create_initbiz_cumuluscore_clusters.php
+++ updates/builder_table_create_initbiz_cumuluscore_clusters.php
@@ -10,7 +10,7 @@ class BuilderTableCreateInitbizCumuluscoreClusters extends Migration
         Schema::create('initbiz_cumuluscore_clusters', function ($table) {
             $table->increments('cluster_id')->unsigned();
             $table->text('full_name');
-            $table->text('slug');
+            $table->text('slug')->unique();
             $table->integer('plan_id')->nullable();
         });
     }
