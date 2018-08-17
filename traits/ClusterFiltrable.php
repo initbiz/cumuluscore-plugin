d5a3af02d10ecd421798d1c7227ed647ef2b3711
Tomasz Strojny <tomasz@init.biz>
2018-08-17 10:45:21 +0200

    Added unique in cluster scope validation rule + minor fixes

--- traits/ClusterFiltrable.php
+++ traits/ClusterFiltrable.php
@@ -20,18 +20,21 @@ trait ClusterFiltrable
      */
     protected function prepareClusterSlug()
     {
+        if ($this->clusterSlug) {
+            return $this->clusterSlug;
+        }
+
         //It is considered secure as cookies created by October are encrypted
         //More info: https://octobercms.com/docs/services/request-input#cookies
 
         //Get current cluster from session, if it fails, get from cookie
-        $clusterSlug = Session::get('cumulus_clusterslug', Cookie::get('cumulus_clusterslug'));
+        $this->clusterSlug = Session::get('cumulus_clusterslug', Cookie::get('cumulus_clusterslug'));
 
         //Cookie may be unset, if so return false
-        if ($clusterSlug) {
-            $this->clusterSlug = $clusterSlug;
-            return true;
+        if (!$this->clusterSlug) {
+            return false;
         }
-        return false;
+        return $this->clusterSlug;
     }
 
     /**
@@ -46,10 +49,35 @@ trait ClusterFiltrable
             return $query->where('cluster_slug', $clusterSlug);
         }
 
-        if (!$this->clusterSlug) {
-            $this->prepareClusterSlug();
-        }
+        $this->prepareClusterSlug();
 
         return $query->where('cluster_slug', $this->clusterSlug);
+    }
+
+    /**
+     * get validation rule of unique in cluster
+     * @param  string $attribute   attribute that have to be unique
+     * @param  string $table       table name, by default will be loaded with $this->table
+     * @param  string $columnName  column with cluster slug
+     * @return string              the rule string
+     */
+    public function clusterUnique($attribute, $table = null, $columnName = 'cluster_slug')
+    {
+        $this->prepareClusterSlug();
+
+        $rule  = 'unique:';
+
+        if ($table) {
+            $rule .= $table;
+        } else {
+            $rule .= $this->table;
+        }
+
+        $rule .= ','.$attribute.',NULL,'.$attribute.','.$columnName.','.$this->clusterSlug;
+
+        // For example: unique:initbiz_exampleplugin_table,email_address,NULL,email_address,cluster_slug,example-cluster
+        // It will check if the cluster has the email_address unique or not
+
+        return $rule;
     }
 }
