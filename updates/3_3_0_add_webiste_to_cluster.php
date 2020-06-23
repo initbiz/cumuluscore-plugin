b97ee028a7c8015d77488c1cd9c2e6716bed53d9
Patryk <patryk@init.biz>
2020-06-23 11:22:23 +0200

    add version

---
+++ updates/3_3_0_add_webiste_to_cluster.php
@@ -0,0 +1,21 @@
+<?php namespace Initbiz\CumulusCore\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class AddWebisteToCluster extends Migration
+{
+    public function up()
+    {
+        Schema::table('initbiz_cumuluscore_clusters', function ($table) {
+            $table->string('webiste')->nullable();
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('initbiz_cumuluscore_clusters', function ($table) {
+            $table->dropColumn('website');
+        });
+    }
+}
