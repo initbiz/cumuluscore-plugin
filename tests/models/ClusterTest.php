b3e1839a454dfc39981a858e7e194fbd98e033c6
Tomasz Strojny <tomasz@init.biz>
2021-03-17 19:33:08 +0100

    Initial tests

---
+++ tests/models/ClusterTest.php
@@ -0,0 +1,272 @@
+<?php
+
+namespace Initbiz\CumulusCore\Tests\Models;
+
+use Initbiz\CumulusCore\Models\Plan;
+use Initbiz\CumulusCore\Models\Cluster;
+use Initbiz\Cumuluscore\Models\ClusterFeatureLog;
+use Initbiz\InitDry\Tests\Classes\FullPluginTestCase;
+
+class ClusterTest extends FullPluginTestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    public function testCanEnterFeature()
+    {
+        $this->assertTrue(true);
+        $cluster = new Cluster;
+        $cluster->name = 'testowy';
+        $cluster->slug= 'testowy';
+        $cluster->save();
+
+        $plan = new Plan;
+        $plan->name = 'testowy plan';
+        $plan->slug = 'testowy-plan';
+        $plan->features = [
+            'initbiz.cumulusdemo.basic.gallery',
+        ];
+        $plan->save();
+
+        $this->assertFalse($cluster->canEnterFeature('initbiz.cumulusdemo.basic.gallery'));
+        $this->assertFalse($cluster->canEnterFeature('initbiz.cumulusdemo.basic.todo'));
+
+        $cluster->plan()->add($plan);
+
+        $this->assertTrue($cluster->canEnterFeature('initbiz.cumulusdemo.basic.gallery'));
+        $this->assertFalse($cluster->canEnterFeature('initbiz.cumulusdemo.basic.todo'));
+    }
+
+    public function testHasFeature()
+    {
+        $this->assertTrue(true);
+        $cluster = new Cluster;
+        $cluster->name = 'testowy';
+        $cluster->slug = 'testowy';
+        $cluster->save();
+
+        $plan = new Plan;
+        $plan->name = 'testowy plan';
+        $plan->slug = 'testowy-plan';
+        $plan->features = [
+            'initbiz.cumulusdemo.basic.gallery',
+        ];
+        $plan->save();
+
+        $this->assertFalse($cluster->hasFeature('initbiz.cumulusdemo.basic.gallery'));
+        $this->assertFalse($cluster->hasFeature('initbiz.cumulusdemo.basic.todo'));
+
+        $cluster->plan()->add($plan);
+
+        $this->assertTrue($cluster->hasFeature('initbiz.cumulusdemo.basic.gallery'));
+        $this->assertFalse($cluster->hasFeature('initbiz.cumulusdemo.basic.todo'));
+    }
+
+    public function testCanEnterAnyFeature()
+    {
+        $cluster = new Cluster;
+        $cluster->name = 'testowy';
+        $cluster->slug = 'testowy';
+        $cluster->save();
+
+        $plan = new Plan;
+        $plan->name = 'testowy plan';
+        $plan->slug = 'testowy-plan';
+        $plan->features = [
+            'initbiz.cumulusdemo.advanced.dashboard',
+            'initbiz.cumulusdemo.advanced.todo',
+            'initbiz.cumulusdemo.advanced.gallery',
+            'initbiz.cumulusdemo.basic.dashboard',
+            'initbiz.cumulussubscriptions.manage_subscription',
+            'initbiz.cumulusplus.cluster_settings',
+        ];
+        $plan->save();
+
+        $this->assertFalse($cluster->canEnterAnyFeature([]));
+        $this->assertFalse($cluster->canEnterAnyFeature(['initbiz.cumulusdemo.advanced.todo', 'initbiz.cumulusdemo.basic.dashboard']));
+
+        $cluster->plan()->add($plan);
+
+        $this->assertTrue($cluster->canEnterAnyFeature(['initbiz.cumulusdemo.advanced.todo', 'initbiz.cumulusdemo.basic.dashboard']));
+        $this->assertTrue($cluster->canEnterAnyFeature(['initbiz.cumulusdemo.advanced.todo', 'initbiz.cumulusdemo.basic.todo']));
+        $this->assertFalse($cluster->canEnterAnyFeature(['initbiz.cumulusdemo.basic.todo', 'initbiz.cumulusdemo.basic.todo']));
+        $this->assertFalse($cluster->canEnterAnyFeature([]));
+    }
+
+    public function testFeatures()
+    {
+        $cluster = new Cluster;
+        $cluster->name = 'testowy';
+        $cluster->slug = 'testowy';
+        $cluster->save();
+
+        $plan = new Plan;
+        $plan->name = 'testowy plan';
+        $plan->slug = 'testowy-plan';
+        $plan->features = [
+            'initbiz.cumulusdemo.advanced.dashboard',
+            'initbiz.cumulusdemo.advanced.todo',
+            'initbiz.cumulusdemo.advanced.gallery',
+            'initbiz.cumulusdemo.basic.dashboard',
+            'initbiz.cumulussubscriptions.manage_subscription',
+            'initbiz.cumulusplus.cluster_settings',
+        ];
+        $plan->save();
+
+        $this->assertNotEquals([
+            'initbiz.cumulusdemo.advanced.dashboard',
+            'initbiz.cumulusdemo.advanced.todo',
+            'initbiz.cumulusdemo.advanced.gallery',
+            'initbiz.cumulusdemo.basic.dashboard',
+            'initbiz.cumulussubscriptions.manage_subscription',
+            'initbiz.cumulusplus.cluster_settings',
+        ], $cluster->features);
+        $this->assertEquals([], $cluster->features);
+
+        $cluster->plan()->add($plan);
+
+        $this->assertEquals([
+            'initbiz.cumulusdemo.advanced.dashboard',
+            'initbiz.cumulusdemo.advanced.todo',
+            'initbiz.cumulusdemo.advanced.gallery',
+            'initbiz.cumulusdemo.basic.dashboard',
+            'initbiz.cumulussubscriptions.manage_subscription',
+            'initbiz.cumulusplus.cluster_settings',
+        ], $cluster->features);
+        $this->assertNotEquals([], $cluster->features);
+    }
+
+    public function testRegisteredFeatures()
+    {
+        $cluster = new Cluster;
+        $cluster->name = 'testowy';
+        $cluster->slug = 'testowy';
+        $cluster->save();
+
+        $plan = new Plan;
+        $plan->name = 'testowy plan';
+        $plan->slug = 'testowy-plan';
+        $plan->features = [
+            'initbiz.cumulusdemo.advanced.dashboard',
+            'initbiz.cumulusdemo.advanced.todo',
+            'initbiz.cumulusdemo.advanced.gallery',
+            'initbiz.cumulusdemo.basic.dashboard',
+            'initbiz.cumulussubscriptions.manage_subscription',
+            'initbiz.cumulusplus.cluster_settings',
+        ];
+        $plan->save();
+
+        $cluster->plan()->add($plan);
+        $cluster->save();
+
+        $this->assertEquals([
+            'initbiz.cumulusdemo.advanced.dashboard',
+            'initbiz.cumulusdemo.advanced.todo',
+            'initbiz.cumulusdemo.advanced.gallery',
+            'initbiz.cumulusdemo.basic.dashboard',
+            'initbiz.cumulussubscriptions.manage_subscription',
+            'initbiz.cumulusplus.cluster_settings',
+        ], $cluster->registered_features);
+        $this->assertNotEquals([], $cluster->registered_features);
+
+        // For registering methods which takes the latest element order desc by timestamp
+        sleep(1);
+        $plan->features = ['initbiz.cumulusdemo.basic.dashboard'];
+        $plan->save();
+        $cluster->save();
+
+        $this->assertNotEquals([], $cluster->registered_features);
+        $this->assertNotEquals('initbiz.cumulusdemo.basic.dashboard', $cluster->registered_features);
+        $this->assertNotEquals([
+            'initbiz.cumulusdemo.advanced.dashboard',
+            'initbiz.cumulusdemo.advanced.todo',
+            'initbiz.cumulusdemo.advanced.gallery',
+            'initbiz.cumulusdemo.basic.dashboard',
+            'initbiz.cumulussubscriptions.manage_subscription',
+            'initbiz.cumulusplus.cluster_settings',
+        ], $cluster->registered_features);
+        $this->assertEquals(['initbiz.cumulusdemo.basic.dashboard'], $cluster->registered_features);
+    }
+
+    public function testRefreshRegisteredFunctions()
+    {
+        $cluster = new Cluster;
+        $cluster->name = 'testowy';
+        $cluster->slug = 'testowy';
+        $cluster->save();
+
+        $plan = new Plan;
+        $plan->name = 'testowy plan';
+        $plan->slug = 'testowy-plan';
+        $plan->features = [
+            'initbiz.cumulusdemo.advanced.dashboard',
+            'initbiz.cumulusdemo.advanced.todo',
+            'initbiz.cumulusdemo.advanced.gallery',
+            'initbiz.cumulusdemo.basic.dashboard',
+            'initbiz.cumulussubscriptions.manage_subscription',
+            'initbiz.cumulusplus.cluster_settings',
+        ];
+        $plan->save();
+
+        $cluster->plan()->add($plan);
+        $cluster->save();
+
+        $this->assertEquals([
+            'initbiz.cumulusdemo.advanced.dashboard',
+            'initbiz.cumulusdemo.advanced.todo',
+            'initbiz.cumulusdemo.advanced.gallery',
+            'initbiz.cumulusdemo.basic.dashboard',
+            'initbiz.cumulussubscriptions.manage_subscription',
+            'initbiz.cumulusplus.cluster_settings',
+        ], $cluster->registered_features);
+        $this->assertNotEquals([], $cluster->registered_features);
+
+        // For registering methods which takes the latest element order desc by timestamp
+        sleep(1);
+        $cluster->refreshRegisteredFeatures(['initbiz.cumulusdemo.basic.dashboard']);
+
+        $this->assertNotEquals([
+            'initbiz.cumulusdemo.advanced.dashboard',
+            'initbiz.cumulusdemo.advanced.todo',
+            'initbiz.cumulusdemo.advanced.gallery',
+            'initbiz.cumulusdemo.basic.dashboard',
+            'initbiz.cumulussubscriptions.manage_subscription',
+            'initbiz.cumulusplus.cluster_settings',
+        ], $cluster->registered_features);
+        $this->assertEquals(['initbiz.cumulusdemo.basic.dashboard'], $cluster->registered_features);
+    }
+
+    public function testRegisterFeature()
+    {
+        $cluster = new Cluster;
+        $cluster->name = 'testowy';
+        $cluster->slug = 'testowy';
+        $cluster->save();
+
+        $cluster->registerFeature('initbiz.cumulusdemo.basic.dashboard');
+        $log = ClusterFeatureLog::where('cluster_slug', 'testowy')->where('feature_code', 'initbiz.cumulusdemo.basic.dashboard')->first();
+
+        $this->assertEquals('registered', $log->action);
+    }
+
+    public function testDeregisterFeature()
+    {
+        $cluster = new Cluster;
+        $cluster->name = 'testowy';
+        $cluster->slug = 'testowy';
+        $cluster->save();
+
+        $cluster->registerFeature('initbiz.cumulusdemo.basic.dashboard');
+        $log = ClusterFeatureLog::where('cluster_slug', 'testowy')->where('feature_code', 'initbiz.cumulusdemo.basic.dashboard')->first();
+
+        $this->assertEquals('registered', $log->action);
+        sleep(1);
+
+        $cluster->deregisterFeature('initbiz.cumulusdemo.basic.dashboard');
+        $log = ClusterFeatureLog::where('cluster_slug', 'testowy')->where('feature_code', 'initbiz.cumulusdemo.basic.dashboard')->orderBy('timestamp', 'desc')->first();
+
+        $this->assertEquals('deregistered', $log->action);
+    }
+}
